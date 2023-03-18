a664b9ab7fcb47330364db943eab6fb4a5b6ec10
maria <mukpere777@gmail.com>
2023-03-18 13:40:11 -0700

    bettychanges

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
 /**
-*Puts
-*C comment
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
