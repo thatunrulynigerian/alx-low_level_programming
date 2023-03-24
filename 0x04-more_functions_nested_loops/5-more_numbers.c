8209fcc032db325e994a6c8996298d1989704223
maria <mukpere777@gmail.com>
2023-03-23 19:03:18 -0700

    changes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,13 +4,13 @@
 /**
  * more_numbers - prints numbers to 14
  *
- * Return: ALways 0 
+ * Return: ALways 0
  */
 
 void more_numbers(void)
 {
 	int n1, n2;
-	
+
 	for (n1 = 0; n1 < 10; n1++)
 	{
 		for (n2 = 0; n2 <= 14; n2++)
