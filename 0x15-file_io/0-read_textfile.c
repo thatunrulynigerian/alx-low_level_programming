050b7480eed0cee57cff7345325787bf114e0313
maria <mukpere777@gmail.com>
2023-05-12 17:45:24 -0700

    file io corrections

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,5 +10,19 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-refer to video https://youtu.be/QrAKV9Eei0M
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
 }
